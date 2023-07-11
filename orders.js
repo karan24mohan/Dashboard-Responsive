2369dbc140878836c2de41e948420156c3b81a70
karan24mohan <karanmohan2410@gmail.com>
2023-07-11 19:31:57 +0530

    first commit

---
+++ orders.js
@@ -0,0 +1,21 @@
+const orders = [{
+        productName: "JavaScript Tutorial",
+        productNumber: "85743",
+        paymentStatus: "Due",
+        status: "Pending",
+    },
+
+    {
+        productName: "CSS Full Course",
+        productNumber: "97245",
+        paymentStatus: "Refunded",
+        status: "Declined",
+    },
+
+    {
+        productName: "Python Tutorial",
+        productNumber: "36452",
+        paymentStatus: "Paid",
+        status: "Active",
+    },
+];
